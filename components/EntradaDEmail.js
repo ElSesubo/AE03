5cd67da413150735e8feb237b32d802f417068e2
DESKTOP-AVKJAFG\Haitian Ji <haitianji123@gmail.com>
2022-11-29 20:02:05 +0100

    Haitian

--- components/EntradaDEmail.js
+++ components/EntradaDEmail.js
@@ -1,29 +1,36 @@
-import { HelperText, TextInput } from 'react-native-paper';
+import { HelperText, TextInput,icons } from 'react-native-paper';
 import { StyleSheet, Text } from 'react-native';
 import { useState } from 'react';
 
 
 function Email() {
-    const [text, setText] = useState();
-
+    const [text, setText] = useState("");
+    const [check,setCheck] = useState("");
     function comparar(){
         if(/^[a-zA-Z]+\@[a-zA-Z\_\-0-9]+\.[a-z]{2,5}$/.test(text)){
             setText(text);
+            setCheck("check");
         }else{
             alert("Falso");
+            setCheck("");
         }
     }
-
+    const checkErrors = () => {
+        return !text.includes("@");
+      };
     return (
         <><TextInput
         style={styles.em}
         label="Email"
         mode="outlined"
         keyboardType="email-address"
-        onSubmitEditing={()=>comparar()}
+        
         onChangeText={text => setText(text)}
-        right={<TextInput.Icon icon="eye" color="#517fa4"/>}/>
-        <Text>{text}</Text>
+        onSubmitEditing={()=>comparar()}
+        right={<TextInput.Icon icon={check} disabled={true}/>}/>
+        <HelperText type="error" visible={checkErrors()}>
+        Email Id is invalid
+      </HelperText>
         </>);
 }
 const styles = StyleSheet.create({
